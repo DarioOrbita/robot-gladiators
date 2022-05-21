b0e2d8cd0beda8202dc6d261f89e616546455522
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-05-21 12:13:16 -0500

    add combat functionality and alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,23 +2,64 @@
 //propmts the user to enter their robot's name.
 var playerName = window.prompt("what is your robot's name?");
 
-console.log(playerName);
+var playerHealth = 100;
 
-console.log("This logs a string, good for when you want to leave yourself a message!");
+var playerAttack = 10;
 
-//does math and logs 20
-console.log(10 + 10);
+//log multiple values at once:
+console.log(playerName, playerHealth, playerAttack);
 
-//Q: What is this?:
-console.log("Our robot's name is " + playerName);
-//A: prints "Our robot's name is (whatever the value of playerName is)" 
+var enemyName = "Roborto"
 
-// this creates a function named "fight"
-function fight() {
+var enemyHealth = 50;
 
-    window.alert("The fight has begun!");
+var enemyAttack = 12;
 
+var fight = function() {
+
+    //alert that the round is starting
+    window.alert("Welcome to Robot Gladiators!");
+
+    //subtract the value of playerAttack from enemyHealth and update enemyHealth
+    enemyHealth = enemyHealth - playerAttack;
+
+    //log the result to see if it worked
+    console.log(
+        playerName + " attacked " + enemyName + " for " + playerAttack + " damage, " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    if (enemyHealth <= 0) {
+
+        window.alert(enemyName + " had died!");
+    } else {
+
+        window.alert(enemyName + " still has " + enemyHealth + " health remaining.")
+    }
+
+    //subtract the value of enemyAttack from playerHealth and update playerHealth
+    playerHealth = playerHealth - enemyAttack;
+
+    //log the result to see if it worked
+    console.log(
+        enemyName + " attacked " + playerName + " for " + enemyAttack + " damage, " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if (playerHealth <= 0) {
+
+        window.alert(playerName + " had died!");
+
+    } else {
+
+        window.alert(playerName + " still has" + playerHealth + " health remaining.");
+        
+    }
+
+
+    if (playerHealth > 0) {
+
+        console.log("Your robot is still alive!");
+
+    }
 }
 
-
-//fight();
+fight();
