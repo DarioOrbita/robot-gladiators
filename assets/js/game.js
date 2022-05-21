2d558c9e6436d997fe72925172fb1bed2203fe1e
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-05-21 11:19:08 -0500

    add name input and console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,24 @@
 
-window.alert("This an alert, JavaScript is running!");
+//propmts the user to enter their robot's name.
+var playerName = window.prompt("what is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for when you want to leave yourself a message!");
+
+//does math and logs 20
+console.log(10 + 10);
+
+//Q: What is this?:
+console.log("Our robot's name is " + playerName);
+//A: prints "Our robot's name is (whatever the value of playerName is)" 
+
+// this creates a function named "fight"
+function fight() {
+
+    window.alert("The fight has begun!");
+
+}
+
+
+//fight();
